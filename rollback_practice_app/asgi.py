7676ade1a58228dffc902715e168d7fc77b88a0a
Moe <moe_akahane@diveintocode.jp>
2022-05-23 17:25:26 +0900

    first commit

--- rollback_practice_app/asgi.py
+++ rollback_practice_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for model_form_app project.
+ASGI config for rollback_practice_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- rollback_practice_app/wsgi.py
+++ rollback_practice_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for model_form_app project.
+WSGI config for rollback_practice_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_form_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rollback_practice_app.settings')
 
 application = get_wsgi_application()
